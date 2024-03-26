b790e158389281d090f897aeee4a007b4d121b2e
LeStegii <paulmertens63@gmail.com>
2024-03-26 11:16:34 +0100

    feat: Update kCommon to 1.4.1 and refactor readme

--- src/main/java/net/kettlemc/example/Plugin.java
+++ src/main/java/net/kettlemc/example/Plugin.java
@@ -1,15 +1,13 @@
 package net.kettlemc.example;
 
-import io.github.almightysatan.slams.minimessage.AdventureMessage;
 import net.kettlemc.example.config.Configuration;
 import net.kettlemc.example.config.Messages;
 import net.kettlemc.example.loading.Loadable;
 import net.kettlemc.kcommon.bukkit.ContentManager;
+import net.kettlemc.kcommon.language.MessageManager;
 import net.kettlemc.klanguage.api.LanguageAPI;
 import net.kettlemc.klanguage.bukkit.BukkitLanguageAPI;
-import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -21,6 +19,7 @@ public final class Plugin implements Loadable {
     private final ContentManager contentManager;
     private final JavaPlugin plugin;
     private BukkitAudiences adventure;
+    private MessageManager messageManager;
 
     public Plugin(JavaPlugin plugin) {
         this.plugin = plugin;
@@ -45,6 +44,9 @@ public final class Plugin implements Loadable {
         if (!Messages.load()) {
             this.plugin.getLogger().severe("Failed to load messages!");
         }
+
+        this.adventure = BukkitAudiences.create(this.plugin);
+        this.messageManager = new MessageManager(Messages.PREFIX, LANGUAGE_API, this.adventure());
     }
 
     @Override
@@ -60,21 +62,15 @@ public final class Plugin implements Loadable {
         return this.adventure;
     }
 
-    public void sendMessage(CommandSender sender, AdventureMessage message) {
-        Audience audience = this.adventure().sender(sender);
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            audience.sendMessage(Messages.PREFIX.value().append(message.value(LANGUAGE_API.getEntity(player))));
-            return;
-        }
-        audience.sendMessage(Messages.PREFIX.value().append(message.value()));
-    }
-
     public JavaPlugin plugin() {
         return plugin;
     }
 
     public ContentManager contentManager() {
         return contentManager;
+    }
+
+    public MessageManager messageManager() {
+        return messageManager;
     }
 }
